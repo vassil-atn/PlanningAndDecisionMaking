8c095b4e7d45dfbda1e5f462a5cc7e0a7b5440d2
Vassil17 <v.v.atanassov@student.tudelft.nl>
2021-12-20 10:53:02 +0100

    Tidy up the steering function a bit, combine movement with rotation

--- simulation_new_steering.py
+++ simulation_new_steering.py
@@ -42,9 +42,10 @@ def integrateNumerically(r,mp_dot,phi_dot,dq,dt):
     theta = r.theta
     q = r.q.copy()
         
-    mp += mp_dot*dt
+    mp = mp + mp_dot*dt
     phi += phi_dot*dt
-    q += dq[:]*dt
+    q = q + dq.T*dt
+
     p[0] = mp[0] + np.cos(phi)*(r.l[0]*np.cos(q[0]) + r.l[1]*np.cos(q[0]+q[1])) - np.sin(phi)*(r.l[0]*np.sin(q[0]) + r.l[1]*np.sin(q[0]+q[1]))
     p[1] = mp[1] + np.sin(phi)*(r.l[0]*np.cos(q[0]) + r.l[1]*np.cos(q[0]+q[1])) + np.cos(phi)*(r.l[0]*np.sin(q[0]) + r.l[1]*np.sin(q[0]+q[1]))
     theta = phi + q[0] + q[1]
@@ -104,8 +105,8 @@ def visualizeMovement(r):
     
 
 # Total time in seconds:
-T = 2
-dt = 0.005 # time step
+T = 5
+dt = 0.01 # time step
 
 plt.figure(1)
 plt.pause(2)
@@ -125,118 +126,94 @@ error_i = 0
 prev_error = 0
 
 # PID CONTROLLER:
-Kp = 3
-Ki = 0.01
-Kd = 0.01
+Kp = 4
+Ki = 0.1
+Kd = 0.005
 
-# Trajectory part 1 - just rotate the phi to the desired direction of movement
+firstRun = np.array([True,True,True])
+
+
+mode = 0
+# Steering function
 for i in range(0,int(T/dt)):
-    X_des = [r.mp[0],r.mp[1],np.arctan2(vector[1],vector[0]),r.q[0],r.q[1]]
-    X = r.phi
 
-    error = X_des[2] - X
-    if abs(error)<0.01*np.pi/180:
-        break
+    vector = np.array([q_des[0]-r.mp[0],q_des[1]-r.mp[1]])/np.linalg.norm([q_des[0]-r.mp[0],q_des[1]-r.mp[1]])    
+    X_des = np.array([q_des[0],q_des[1],np.arctan2(vector[1],vector[0]),q_des[3],q_des[4]])
+    X = np.array([r.mp[0],r.mp[1],r.phi,r.q[0],r.q[1]])
+    
+    # First make sure the heading to the configuration is correct
+    if abs(X_des[2] - X[2]) > 0.1 and mode != 3:
+        if firstRun[0] == True:
+            error_i = 0
+            prev_error = 0
+        firstRun[0] = False
+# =============================================================================
+#         firstRun[1:3] = True
+# =============================================================================
+        error = np.zeros([5])
+        error[2] = X_des[2] - X[2]
+        print("mode 1")
+        mode = 1
+
+
+        #
+    # If the heading is correct check the xy position of the base and the joint angles:
+    elif np.any(abs(X_des[0:2] - X[0:2]) > 0.1) or np.any(abs(X_des[3:5] - X[3:5]) > 0.1):
+        if firstRun[1] == True:
+            error_i = 0
+            prev_error = 0
+        firstRun[1] = False
+# =============================================================================
+#         firstRun[0] = True
+#         firstRun[2] = True
+# =============================================================================
+        error = np.zeros([5])
+        error = X_des - X
+        error[2] = 0
+        print("mode 2")
+        mode = 2
+    # Finally ensure that the robot rotates to the desired phi
+    elif abs(q_des[2]- X[2])>0.1:
+        X_des = np.array([q_des[0],q_des[1],q_des[2],q_des[3],q_des[4]])
+        if firstRun[2] == True:
+            error_i = 0
+            prev_error = 0
+        firstRun[2] = False
+# =============================================================================
+#         firstRun[0:2] = True
+# =============================================================================
+        error = np.zeros([5])
+        error[2] = X_des[2] - X[2]
+        mode = 3
+        print("mode 3")
+    
+    
     error_d = (prev_error - error)
     error_i = error_i + error
-
-    mp_dot = np.zeros(2)
-    phi_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
-    dq = np.zeros(2)
-    
-    u = np.array([-phi_dot*r.h,phi_dot*r.h])
-    if abs(u[0])>r.u_limits and u[0]<0:
-        u[0] = -r.u_limits
-        u[1] = r.u_limits
-    if abs(u[0])>r.u_limits and u[1]<0:
-        u[0] = r.u_limits
-        u[1] = -r.u_limits
-    
-    phi_dot = (u[1]-u[0])/(2*r.h)
-    
-    # Integrate numerically
-    mp,phi,q,p,theta,p_joint_1 = integrateNumerically(r,mp_dot,phi_dot,dq,dt)
-    
-    r.Update(mp,phi,p,theta,q,dq,np.array([0.0,0.0]),p_joint_1) 
-    
-    # Visualize the movement
-    visualizeMovement(r)
-    storeP = np.append(storeP,[p],axis=0)
-    # Save error for the derivative controller
-    prev_error = error
-    
-
-# Trajectory part 2 - move the mobile robot in straight line
-prev_error = [0,0]
-error_i = [0,0]
-for i in range(0,int(T/dt)):
-    X_des = [q_des[0],q_des[1],r.phi,r.q[0],r.q[1]]
-    X = np.array([r.mp[0],r.mp[1]])
-    
-    error = X_des[0:2] - X
-    if (np.absolute(error)<0.0001).all():
-        break
-    error_d = (prev_error - error)
-    error_i = error_i + error
-    
-    mp_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
-    phi_dot = 0
-    dq = np.zeros(2)
-    
-    u = np.array([[np.linalg.norm(mp_dot)],[np.linalg.norm(mp_dot)]])
-    if u[0]>r.u_limits:
-        u = np.array([[r.u_limits],[r.u_limits]])
         
-    mp_dot = np.array([[np.cos(r.phi)/2,np.cos(r.phi)/2],[np.sin(r.phi)/2,np.sin(r.phi)/2]])@u
-    mp_dot = np.reshape(mp_dot,2)
-    
-    # Integrate numerically
-    mp,phi,q,p,theta,p_joint_1 = integrateNumerically(r,mp_dot,phi_dot,dq,dt)
-    
-    r.Update(mp,phi,p,theta,q,dq,np.array([0.0,0.0]),p_joint_1) 
-    
-    # Visualize the movement
-    visualizeMovement(r)
-    storeP = np.append(storeP,[p],axis=0)
-    # Save error for the derivative controller
-    prev_error = error
 
-# Trajectory part 3 - rotate phi and [q1,q2] to the desired values
-prev_error = 0
-error_i = 0    
-for i in range(0,int(T/dt)):
-    X_des = [r.mp[0],r.mp[1],q_des[2],q_des[3],q_des[4]]
-    X = np.array([r.phi,r.q[0],r.q[1]])
+    X_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
+
+
+    mp_dot_des = X_dot[0:2]
+
+    phi_dot_des = X_dot[2]
+    dq_des = X_dot[3:5]
     
-    error = X_des[2:] - X
-    if (np.absolute(error)<0.01*np.pi/180).all():
-        break
-    error_d = (prev_error - error)
-    error_i = error_i + error
+    # If the robot needs to move to target the u_des is different than when it needs to rotate:
+    if mode == 1 or mode == 3:
+        u_des = np.array([-phi_dot_des*r.h,phi_dot_des*r.h])
+    else:
+        u_des = np.array([np.linalg.norm(mp_dot_des),np.linalg.norm(mp_dot_des)])
     
-    mp_dot = np.zeros(2)
-    Q_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
-    phi_dot = Q_dot[0]
-    dq = Q_dot[1:]
     
-    u = np.array([-phi_dot*r.h,phi_dot*r.h])
-    if abs(u[0])>r.u_limits and u[0]<0:
-        u[0] = -r.u_limits
-        u[1] = r.u_limits
-    if abs(u[0])>r.u_limits and u[1]<0:
-        u[0] = r.u_limits
-        u[1] = -r.u_limits
-    if abs(dq[0])>r.dq_limits and dq[0]>0:
-        dq[0] = r.dq_limits
-    if abs(dq[0])>r.dq_limits and dq[0]<0:
-        dq[0] = -r.dq_limits
-    if dq[1]>r.dq_limits and dq[1]>0:
-        dq[1] = r.dq_limits
-    if dq[1]>r.dq_limits and dq[1]<0:
-        dq[1] = -r.dq_limits
+    u_des = np.clip(u_des,-r.u_limits,r.u_limits)
     
-    phi_dot = (u[1]-u[0])/(2*r.h)
-    
+    # Simulate the movement:
+    phi_dot = (u_des[1]-u_des[0])/(2*r.h)
+    mp_dot = ((np.array([[np.cos(r.phi)/2,np.cos(r.phi)/2],[np.sin(r.phi)/2,np.sin(r.phi)/2]])).dot(u_des)).T
+    #mp_dot = np.reshape(mp_dot,2)
+    dq = dq_des.copy()
     # Integrate numerically
     mp,phi,q,p,theta,p_joint_1 = integrateNumerically(r,mp_dot,phi_dot,dq,dt)
     
@@ -246,7 +223,111 @@ for i in range(0,int(T/dt)):
     visualizeMovement(r)
     storeP = np.append(storeP,[p],axis=0)
     # Save error for the derivative controller
-    prev_error = error
+    prev_error = error       
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# 
+#     
+# 
+# # Trajectory part 2 - move the mobile robot in straight line
+# prev_error = [0,0]
+# error_i = [0,0]
+# for i in range(0,int(T/dt)):
+#     X_des = [q_des[0],q_des[1],r.phi,r.q[0],r.q[1]]
+#     X = np.array([r.mp[0],r.mp[1]])
+#     
+#     error = X_des[0:2] - X
+#     if (np.absolute(error)<0.01).all():
+#         break
+#     error_d = (prev_error - error)
+#     error_i = error_i + error
+#     
+#     mp_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
+#     phi_dot = 0
+#     dq = np.zeros(2)
+#     
+#     u = np.array([[np.linalg.norm(mp_dot)],[np.linalg.norm(mp_dot)]])
+#     if u[0]>r.u_limits:
+#         u = np.array([[r.u_limits],[r.u_limits]])
+#         
+#     mp_dot = np.array([[np.cos(r.phi)/2,np.cos(r.phi)/2],[np.sin(r.phi)/2,np.sin(r.phi)/2]])@u
+#     mp_dot = np.reshape(mp_dot,2)
+#     
+#     # Integrate numerically
+#     mp,phi,q,p,theta,p_joint_1 = integrateNumerically(r,mp_dot,phi_dot,dq,dt)
+#     
+#     r.Update(mp,phi,p,theta,q,dq,np.array([0.0,0.0]),p_joint_1) 
+#     
+#     # Visualize the movement
+#     visualizeMovement(r)
+#     storeP = np.append(storeP,[p],axis=0)
+#     # Save error for the derivative controller
+#     prev_error = error
+# 
+# # Trajectory part 3 - rotate phi and [q1,q2] to the desired values
+# prev_error = 0
+# error_i = 0    
+# for i in range(0,int(T/dt)):
+#     X_des = [r.mp[0],r.mp[1],q_des[2],q_des[3],q_des[4]]
+#     X = np.array([r.phi,r.q[0],r.q[1]])
+#     
+#     error = X_des[2:] - X
+#     if (np.absolute(error)<0.01*np.pi/180).all():
+#         break
+#     error_d = (prev_error - error)
+#     error_i = error_i + error
+#     
+#     mp_dot = np.zeros(2)
+#     Q_dot = Kp*error + Ki*error_i*dt + Kd*error_d/dt
+#     phi_dot = Q_dot[0]
+#     dq = Q_dot[1:]
+#     
+#     u = np.array([-phi_dot*r.h,phi_dot*r.h])
+#     if abs(u[0])>r.u_limits and u[0]<0:
+#         u[0] = -r.u_limits
+#         u[1] = r.u_limits
+#     if abs(u[0])>r.u_limits and u[1]<0:
+#         u[0] = r.u_limits
+#         u[1] = -r.u_limits
+#     if abs(dq[0])>r.dq_limits and dq[0]>0:
+#         dq[0] = r.dq_limits
+#     if abs(dq[0])>r.dq_limits and dq[0]<0:
+#         dq[0] = -r.dq_limits
+#     if dq[1]>r.dq_limits and dq[1]>0:
+#         dq[1] = r.dq_limits
+#     if dq[1]>r.dq_limits and dq[1]<0:
+#         dq[1] = -r.dq_limits
+#     
+#     phi_dot = (u[1]-u[0])/(2*r.h)
+#     
+#     # Integrate numerically
+#     mp,phi,q,p,theta,p_joint_1 = integrateNumerically(r,mp_dot,phi_dot,dq,dt)
+#     
+#     r.Update(mp,phi,p,theta,q,dq,np.array([0.0,0.0]),p_joint_1) 
+#     
+#     # Visualize the movement
+#     visualizeMovement(r)
+#     storeP = np.append(storeP,[p],axis=0)
+#     # Save error for the derivative controller
+#     prev_error = error
+# =============================================================================
 
 # Plot the trajectory of the end-effector
 plt.plot(storeP[:,0],storeP[:,1],'--')
